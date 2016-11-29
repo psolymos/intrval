51d1f077adfd1aab77d378843cf0793f3aab92b4
Peter Solymos <psolymos@gmail.com>
2016-11-28 17:09:07 -0700

    break or not?

--- R/intrval_types.R
+++ R/intrval_types.R
@@ -1,17 +1,27 @@
 intrval_types <- function(type=NULL) {
     tab <- matrix(c(
-        "x %[]% c(a, b)", "---x===x---", "x >= a & x <= b",
-        "x %[)% c(a, b)", "---x===o---", "x >= a & x <  b",
-        "x %(]% c(a, b)", "---o===x---", "x >  a & x <= b",
-        "x %()% c(a, b)", "---o===o---", "x >  a & x <  b",
+        "x %[]%  c(a, b)",  "---x===x---", "x >= a & x <= b",
+        "x %)(%  c(a, b)",  "===o---o===", "x < a | x > b",
+        "x %[<]% c(a, b)",  "===o---o---", "x < a",
+        "x %[>]% c(a, b)",  "---o---o===", "x > b",
 
-        "x %][% c(a, b)", "===x---x===", "x <= a & x >= b",
-        "x %](% c(a, b)", "===x---o===", "x <= a & x >  b",
-        "x %)[% c(a, b)", "===o---x===", "x <  a & x >= b",
-        "x %)(% c(a, b)", "===o---o===", "x <  a & x >  b"),
-        8, 3, byrow=TRUE)
-    colnames(tab) <- c("Expression", "Visual", "Meaning")
-    rownames(tab) <- substr(tab[,1L], 4, 5)
+        "x %[)%  c(a, b)",  "---x===o---", "x >= a & x < b",
+        "x %)[%  c(a, b)",  "===o---x===", "x < a | x >= b",
+        "x %[<)% c(a, b)",  "===o---o---", "x < a",
+        "x %[>)% c(a, b)",  "---o---x===", "x >= b",
+
+        "x %(]%  c(a, b)",  "---o===x---", "x > a & x <= b",
+        "x %](%  c(a, b)",  "===x---o===", "x <= a | x > b",
+        "x %(<]% c(a, b)",  "===x---o---", "x <= a",
+        "x %(>]% c(a, b)",  "---o---o===", "x > b",
+
+        "x %()%  c(a, b)",  "---o===o---", "x >  a & x < b",
+        "x %][%  c(a, b)",  "===x---x===", "x <= a | x >= b",
+        "x %(<)% c(a, b)",  "===x---o---", "x <= a",
+        "x %(>)% c(a, b)",  "---o---x===", "x >= b"),
+        ncol=3, byrow=TRUE)
+    colnames(tab) <- c("Expression", "Visual", "Condition")
+    rownames(tab) <- substr(tab[,1L], 3, nchar(tab[,1L])-8)
     if (!is.null(type))
         tab <- tab[type,,drop=FALSE]
     tab
--- R/print_intrval.R
+++ R/print_intrval.R
@@ -1,5 +1,5 @@
 print_intrval <- function(type=NULL) {
     tab <- intrval_types(type=type)
-    print(as.data.frame(tab), print.gap=4)
+    print(as.data.frame(tab), print.gap=4, right=FALSE)
     invisible(tab)
 }
