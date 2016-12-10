43ccd588253171bea8a74d0da1a328a2a61a0c99
mgacc0 <mgacc0@users.noreply.github.com>
2016-12-10 17:11:14 +0100

    updated function .intrval3
    The previous attempts of writing the function .intrval3 had many bugs.
    This includes some tests (that should be placed in the appropriate file).

--- extras/playground_operators.R
+++ extras/playground_operators.R
@@ -1,104 +1,116 @@
-is_empty_interval <- function(interval, type) {
-  interval[[1]]==interval[[2]] & type=="()"
-}
-
-overlaps <- function (interval1, interval2, type1, type2)
-{
-    stopifnot(is.vector(interval1) && length(interval1)<=2)
-    stopifnot(is.vector(interval2) && length(interval2)<=2)
-
-    iv1 <- .get_intrval(interval1)
-    iv2 <- .get_intrval(interval2)
-
-    type1 <- match.arg(type1, c("[]", "[)", "(]", "()"))
-    type2 <- match.arg(type2, c("[]", "[)", "(]", "()"))
-
-    if (is_empty_interval(iv1, type1) | is_empty_interval(iv2, type2)) {
-      return(FALSE)
-    }
-
-
-    if (iv1$a > iv2$a) {
-        tmp <- iv1
-        iv1 <- iv2
-        iv2 <- tmp
-
-        tmp <- type1
-        type1 <- type2
-        type2 <- tmp
-    }
-
-    !.lssthan(iv1$b, iv2,
-        ifelse(substr(type1, 2L, 2L)=="]" && substr(type2, 1L, 1L)=="[",
-        "[", "("))
-}
-
-
-
-
-library(dplyr, purrr)
-
-mm <- expand.grid(
-  iv1_a=c(1, 2, 3, 4, 5),
-  iv1_b=c(1, 2, 3, 4, 5),
-  iv2_a=c(1, 2, 3, 4, 5),
-  iv2_b=c(1, 2, 3, 4, 5),
-  type1=c("[]", "[)", "(]", "()"),
-  type2=c("[]", "[)", "(]", "()"),
-  stringsAsFactors=FALSE)
-nrow(mm)
-
-mm %>%
-  sample_n(4)
-
-mm %>%
-  sample_n(10) %>%
-  by_row(~ overlaps(c(.[[1]], .[[2]]), c(.[[3]], .[[4]]),
-                    .[[5]], .[[6]]), .collate="cols")
-
-mm %>%
-  .[, 1:4] %>%
-  sample_n(10) %>%
-  by_row(~ c(.[[1]], .[[2]]) %[]o[]% c(.[[3]], .[[4]]), .collate="cols")
-
-
-
 library(testthat)
-expect_false(overlaps(c(2, 4), c(2, 2), "[]", "()"))
-expect_false(overlaps(c(2, 2), c(2, 4), "()", "[]"))
 
 
-
-intrval4 <- function (df1)
-{
-  stopifnot(is.data.frame(df1))
-  stopifnot(nrow(df1)==1)
-
-  iv1 <- .get_intrval(c(df1$iv1_a, df1$iv1_b))
-  iv2 <- .get_intrval(c(df1$iv2_a, df1$iv2_b))
-
-  type1 <- match.arg(df1$type1, c("[]", "[)", "(]", "()"))
-  type2 <- match.arg(df1$type2, c("[]", "[)", "(]", "()"))
-
-  if (is_empty_interval(iv1, type1) | is_empty_interval(iv1, type1)) {
-    return (FALSE)
-  }
-
-  if (iv1$a > iv2$a) {
-    tmp <- iv1
-    iv1 <- iv2
-    iv2 <- tmp
-
-    tmp <- type1
-    type1 <- type2
-    type2 <- tmp
-  }
-
-  !.lssthan(iv1$b, iv2,
-            ifelse(substr(type1, 2L, 2L)=="]" && substr(type2, 1L, 1L)=="[",
-                   "[", "("))
+.reverse_type <- function (type) {
+  ifelse(type=="(]", "[)",
+         ifelse(type=="[)", "(]", type))
 }
 
-mm %>%
-  sample_n(20) %>%
-  by_row(intrval4, .collate=c("cols"))
+.is_empty_interval <- function(interval, type) {
+  interval[[1]]==interval[[2]] & type %in% c("()", "[)", "(]")
+}
+
+
+.intrval <- function (x, interval, type) {
+  print(x)
+  print(interval)
+  print(type)
+
+  type <- lapply(type, function(x)
+    match.arg(x, c("[]", "[)", "(]", "()", "][", "](", ")[", ")(")))
+  type <- ifelse(interval[[1]]<=interval[[2]], type, .reverse_type(type))
+
+  type_a <- substr(type, 1L, 1L)
+  type_b <- substr(type, 2L, 2L)
+  ab <- .get_intrval(interval)
+  A <- ifelse(type_a=="[", x >= ab$a,
+              ifelse(type_a=="]",x <= ab$a,
+                     ifelse(type_a=="(", x > ab$a,
+                            ifelse(type_a==")", x < ab$a,
+                                   NA))))
+  B <- ifelse(type_b=="[", x >= ab$b,
+              ifelse(type_b=="]", x <= ab$b,
+                     ifelse(type_b=="(", x > ab$b,
+                            ifelse(type_b==")", x < ab$b,
+                                   NA))))
+  print(A & B)
+  A & B
+}
+expect_false(.intrval(4, c(4, 5), "(]"))
+expect_false(.intrval(4, c(5, 4), "[)"))
+
+
+.get_intrval <- function(interval) {
+  if (!is.null(dim(interval))) {
+    if (ncol(interval) > 2L)
+      warning("only first 2 columns of interval object are used")
+  } else {
+    if (length(interval) > 2L)
+      warning("only first 2 elements of interval object are used")
+  }
+
+  list(a=pmin(interval[[1]], interval[[2]], na.rm=FALSE),
+       b=pmax(interval[[1]], interval[[2]], na.rm=FALSE))
+}
+expect_warning(.get_intrval(c(5, 3, 3)))
+expect_warning(.get_intrval(as.list(c(5, 3, 3), c(2, 1))))
+expect_warning(.get_intrval(data.frame(c(5, 3), c(2, 1), c(1, 1))))
+
+
+.intrval3 <- function(interval1, interval2, type1, type2)
+{
+  iv1 <- .get_intrval(interval1)
+  iv2 <- .get_intrval(interval2)
+
+  type1 <- lapply(type1, function(x) match.arg(x, c("[]", "[)", "(]", "()")))
+  type2 <- lapply(type2, function(x) match.arg(x, c("[]", "[)", "(]", "()")))
+
+  type1 <- ifelse(interval1[[1]]<=interval1[[2]], type1, .reverse_type(type1))
+  type2 <- ifelse(interval2[[1]]<=interval2[[2]], type2, .reverse_type(type2))
+
+  ifelse(.is_empty_interval(iv1, type1) | .is_empty_interval(iv2, type2),
+         FALSE,
+         ifelse(iv1$a==iv2$a & iv1$b==iv2$b, TRUE,
+                ifelse(iv1$a < iv2$a | (iv1$a==iv2$a & iv1$b < iv2$b),
+                       .intrval(iv1$b, iv2,
+                                ifelse(substr(type1, 2, 2)=="]", type2, "(]"))
+                       | .intrval(iv2$a, iv1,
+                                  ifelse(substr(type2, 1, 1)=="[", type1, "[)")),
+                       .intrval(iv2$b, iv1,
+                                ifelse(substr(type2, 2, 2)=="]", type1, "(]"))
+                       | .intrval(iv1$a, iv2,
+                                  ifelse(substr(type1, 1, 1)=="[", type2, "[)"))
+         )))
+}
+
+
+
+expect_false(.intrval3(c(4, 5), c(4, 1), "[)", "(]"))
+
+expect_false(.intrval3(c(2, 5), c(1, 2), "()", "()"))
+
+expect_true(.intrval3(c(5, 3), c(4, 5), "[]", "[)"))
+expect_true(.intrval3(c(4, 5), c(5, 3), "[)", "[]"))
+expect_true(.intrval3(c(3, 5), c(4, 5), "[]", "[)"))
+
+expect_true(.intrval3(c(3, 4), c(3, 4), "(]", "[)"))
+expect_true(.intrval3(c(3, 4), c(3, 4), "[)", "(]"))
+
+expect_true(.intrval3(c(1, 3), c(1, 5), "[]", "[]"))
+
+expect_false(.intrval3(c(2, 5), c(2, 1), "[]", "()"))
+
+expect_false(.intrval3(c(4, 4), c(5, 4), "[]", "()"))
+expect_false(.intrval3(c(4, 4), c(4, 5), "[]", "()"))
+
+expect_false(.intrval3(c(4, 4), c(4, 5), "[]", "(]"))
+expect_false(.intrval3(c(4, 4), c(5, 4), "[]", "[)"))
+
+expect_false(.intrval3(c(2, 4), c(2, 2), "[]", "()"))
+expect_false(.intrval3(c(2, 2), c(2, 4), "()", "[]"))
+
+expect_equal( .intrval3(c(2, 4), c(2, 2), "[]", "()"),
+              .intrval3(c(2, 2), c(2, 4), "()", "[]") )
+
+
+
